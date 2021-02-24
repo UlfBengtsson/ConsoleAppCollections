3a45bdf9de11777b4890d93eab8ae0552b53dc08
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-02-24 11:27:15 +0100

    end of code show

--- ConsoleAppCollections/Program.cs
+++ ConsoleAppCollections/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Text;// needed to use StringBuilder
 
 namespace ConsoleAppCollections
 {
     class Program
     {
+        static Random randomGenerator = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] numbers = new int[3];
+
+            Console.WriteLine(numbers);
+
+        }
+
+        static void NameEx()
+        {
+            Console.WriteLine("Random first name generated: " + RandomFirstName());
+            Console.WriteLine("Random last name generated: " + RandomLastName());
+            Console.WriteLine("Random full name generated: " + RandomFullName());
+        }
+
+        static string RandomFullName()
+        {
+            return RandomFirstName() + ' ' + RandomLastName();
+        }
+
+        static string RandomLastName()
+        {
+            string[] names = { "Bengtsson", "Svensson", "Karlsson",};
+
+            return names[randomGenerator.Next(names.Length)];
+        }
+
+        static string RandomFirstName()
+        {
+            string[] names = { "Ulf", "Eva", "Kent", "Anna", "Sven" };
+
+            return names[randomGenerator.Next(names.Length)];
+        }
+
+        static void BasicRandom()
+        {
+            Random rng = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                //Console.WriteLine(random.Next());//random next int 0 to int.max 2.1 B
+                //Console.WriteLine(random.Next(500));//random next with max limit 0-499
+                Console.WriteLine(rng.Next(100, 500));//random next with min/max limit 100-499
+                // min is included to numbers possible, max is excluded f
+            }
+        }
 
             //string[] collectionOfNames = new string[2];
 
@@ -16,10 +63,76 @@ namespace ConsoleAppCollections
 
             //PrintStringArray(collectionOfNames);
 
-            AskUserForNumber("fuel prize");
+            //AskUserForNumber("fuel prize");
 
             //ArrayEx();
             //PrimitiveNotChangeEx();
+
+
+        static void SpeedTestForString()
+        {
+            string manyStringStars = "";
+
+            DateTime startStringTime = DateTime.Now;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                manyStringStars = manyStringStars + "*";
+            }
+
+            DateTime endStringTime = DateTime.Now;
+
+            Console.WriteLine("String time was: " + (endStringTime - startStringTime).TotalMilliseconds);
+
+            StringBuilder many_SB_Stars = new StringBuilder();
+
+            DateTime startSBTime = DateTime.Now;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                many_SB_Stars.Append("*");
+            }
+
+            DateTime endSBTime = DateTime.Now;
+
+            Console.WriteLine("StringBuilder time was: " + (endSBTime - startSBTime).TotalMilliseconds);
+        }
+
+        static void StringCompeareingV2()
+        {
+            string name = "ulf";
+
+            StringBuilder sameName = new StringBuilder();
+
+            sameName.Append('u');
+            sameName.Append('l');
+            sameName.Append('f');
+
+            //if (name.Equals(sameName.ToString()))// == is not garenteed to work for string every time
+            if (name == sameName.ToString())// == is not garenteed to work for string every time
+            {
+                Console.WriteLine("works");
+            }
+            else
+            {
+                Console.WriteLine("incorrect");
+            }
+        }
+        static void StringCompeareing()
+        {
+            string name = Console.ReadLine();
+
+            string sameName = Console.ReadLine();
+
+
+            if (name.Equals(sameName))// == is not garenteed to work for string every time
+            {
+                Console.WriteLine("== works");
+            }
+            else
+            {
+                Console.WriteLine("== incorrect");
+            }
         }
 
         static double AskUserForNumber(string whatNumber)
@@ -48,10 +161,10 @@ namespace ConsoleAppCollections
                     Console.WriteLine("The number was too big for this program to handle.");
                 }
 
-            } while (notANumber);
-
+            } while (notANumber);//end of do while loop
+            
             return number;
-        }
+        }// end of AskUserFor
 
 
         static void PrimitiveNotChangeEx()
